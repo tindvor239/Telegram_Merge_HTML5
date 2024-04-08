cdec7e54c0b2dda982d54624bcfaf0f567187656
An Vuong <37416837+tindvor239@users.noreply.github.com>
2024-04-08 11:19:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const bot = new TelegramBot(TOKEN, {
     polling: true
 });
 const port = process.env.PORT || 5000;
-const gameName = "CatMerge";
+const gameName = "Telegram_Merge_HTML5";
 const queries = {};
-server.use(express.static(path.join(__dirname, 'CatMerge')));
+server.use(express.static(path.join(__dirname, 'Telegram_Merge_HTML5')));
 bot.onText(/help/, (msg) => bot.sendMessage(msg.from.id, "Say /game if you want to play."));
 bot.onText(/start|game/, (msg) => bot.sendGame(msg.from.id, gameName));
 bot.on("callback_query", function (query) {
